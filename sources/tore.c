41e918b8750ffe4f3246e208cb1b71207ca175c2
Gasparotto <quentin.gasparotto@epitech.eu>
2015-03-20 11:09:31 +0100

    degré 4 resolu en attente de test

--- sources/tore.c
+++ sources/tore.c
@@ -5,7 +5,7 @@
 ** Login   <gaspar_q@epitech.net>
 ** 
 ** Started on  Thu Mar 19 19:30:35 2015 quentin gasparotto
-** Last update Thu Mar 19 20:30:02 2015 quentin gasparotto
+** Last update Fri Mar 20 11:09:10 2015 quentin gasparotto
 */
 
 #include "../include/prototypes.h"
@@ -52,43 +52,105 @@ void	init_second_function(double *param, double *ex_param)
 		   param[1] * param[2] / 4 + param[3] / 2), 2));
 }
 
+double		get_min(double *roots, int size)
+{
+  int		rank;
+  double	final;
+
+  final = -1.0;
+  rank = 0;
+  while (rank < size)
+    {
+      if ((roots[rank] < 0 && roots[rank] < final) ||
+	  (roots[rank] > 0 && final == -1.0))
+	final = roots[rank];
+      rank = rank + 1;
+    }
+  return (final);
+}
+
 double		draw_tore(t_streight strgt, t_object *my_obj)
 {
   double	param[5];
   double	param2[4];
+  double	*roots;
+  double	d;
+  double	d_prime;
+  double	alpha;
+  double	my_roots[4];
+  double	k_roots[2];
+  t_vector3	param1;
 
   init_tore_param(param, strgt, my_obj);
-  if (param[0] == 0)
-    //résoud l'équation de dégré 3
+  if (param[0] == 0 && param[1] == 0)
+    {
+      param1.x = param[2];
+      param1.y = param[3];
+      param1.z = param[4];
+      resolve_polynoms(param1, k_roots);
+      return (get_min(k_roots, roots, 2));
+    }
+  else if (param[0] == 0)
+    {
+      roots = cubic_solver(param);
+      return (get_min(roots, 2));
+    }
   else
     {
       init_second_function(param2, param);
-      //résoud l'équation de dégré 3
-      if (/* des racine */)
+      roots = cubic_solver(param2);
+      if (roots[0] != -1.0)
 	{
-	  //on prend une racine à la wanne again et on se tape la bite contre le clavier
-	  //s'il s'avère qu'un front se trouve dans la trajectoire de votre bite à se moment là
-	  //considérez que vous vous tapez la bite contre un front
-	  //on calc un delta
-	  if (/* delta < 0 */)
+	  d = 2.0 * (pow(roots[0], 2) + param2[1]);
+	  if (d < 0)
+	    return (-1.0);
+	  else if (d == 0)
 	    {
-	      //no root
-	    }
-	  else if (/* delta = 0 */)
-	    {
-	      //d'
-	      if (/* d' < 0 */)
-		{
-		  //no root
-		}
+	      d_prime = pow(roots[0], 2) + param2[2];
+	      if (d_prime < 0)
+		return (-1.0);
 	      else
-		//d'
+		d_prime = sqrt(d_prime);
+	    }
+	  else
+	    d_prime = sqrt((param2[1] * param2[2] - param2[3]) / d);
+
+	  alpha = d - 4.0 * (roots[0] - d_prime);
+	  if (alpha == F_ZERO)
+	    {
+	      roots[0] = -sqrt(d) / 2.0 + param[0] / 4.0;
+	      roots[1] = -1.0;
+	    }
+	  else if (alpha > F_ZERO)
+	    {
+	      my_roots[0] = -(sqrt(d) + sqrt(alpha)) / 2.0 - param[0] / 4.0;
+	      my_roots[1] = -(sqrt(d) - sqrt(alpha)) / 2.0 - param[0] / 4.0;
 	    }
 	  else
 	    {
-	      //d'
+	      my_roots[0] = -1.0;
+	      my_roots[1] = -1.0;
 	    }
-	  //calc des racines
+
+	  alpha = d - 4.0 * (roots[0] + d_prime);
+	  if (alpha == 0)
+	    {
+	      my_roots[2] = sqrt(d) / 2 + param[0] / 4.0;
+	      my_roots[3] = -1.0;
+	    }
+	  else if (alpha > 0)
+	    {
+	      my_roots[2] = (sqrt(d) + sqrt(alpha)) / 2 - param[0] / 4.0;
+	      my_roots[3] = (sqrt(d) - sqrt(alpha)) / 2 - param[0] / 4.0;
+	    }
+	  else
+	    {
+	      my_roots[2] = -1.0;
+	      my_roots[3] = -1.0;
+	    }
+	  return (get_min(my_roots, 4));
 	}
+      else
+	return (-1.0);
     }
 }
