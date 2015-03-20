a23567060a92d660be4b392569a56e1db888e188
fernand veyrier <veyrie_f@pc-veyrie_f>
2015-03-20 13:38:02 +0100

    toto

--- cubic_solver.c
+++ cubic_solver.c
@@ -5,7 +5,7 @@
 ** Login   <veyrie_f@epitech.net>
 ** 
 ** Started on  Thu Mar 19 19:57:46 2015 fernand veyrier
-** Last update Fri Mar 20 11:07:06 2015 fernand veyrier
+** Last update Fri Mar 20 13:37:34 2015 fernand veyrier
 */
 
 #include <stdlib.h>
@@ -17,6 +17,7 @@ double		*cubic_solver(double *param)
   double	q;
   double	delta;
   double	*result;
+  double	alpha;
 
   ((result = malloc(sizeof(int) * 3)) == NULL) ? exit(-1) : 0;
   result[0] = -1;
@@ -40,18 +41,24 @@ double		*cubic_solver(double *param)
 	}
       result[0] = (3 * q) / p - (param[1] / (3 * param[0]));
       result[1] = (-3 * q) / (2 * p) - (param[1] / (3 * param[0]));
+      result[2] = result[1];
       return (result);
     }
   else
     {
+      alpha = 1 / 3 * acos(delta);
+      result[0] = -param[1] / (3 * param[0]) + 2 * sqrt(-p / 3) * cos(alpha);
+      result[1] = -param[1] / (3 * param[0]) + 2 * sqrt(-p / 3) * cos(alpha + (2 * M_PI) / 3);
+      result[2] = -param[1] / (3 * param[0]) + 2 * sqrt(-p / 3) * cos(alpha + (4 * M_PI) / 3);
       /* Et là, c'est le drame. */
+      printf("z1 = %f z2 = %f z3 = %f\n", result[0], result[1], result[2]);
       return (result);
     }
 }
 
 int		main(int ac, char **av)
 {
-  double	param[4] = {-3, -3, -2, 5};
+  double	param[4] = {-2, 3, 0, -1};
 
   cubic_solver(param);
 }
