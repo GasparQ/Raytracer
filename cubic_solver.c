ad9e17aae6cb2a6cdaf7fc69279d5bd5c824576f
fernand veyrier <veyrie_f@pc-veyrie_f>
2015-03-20 15:58:37 +0100

    TOTO

--- cubic_solver.c
+++ cubic_solver.c
@@ -5,7 +5,7 @@
 ** Login   <veyrie_f@epitech.net>
 ** 
 ** Started on  Thu Mar 19 19:57:46 2015 fernand veyrier
-** Last update Fri Mar 20 13:37:34 2015 fernand veyrier
+** Last update Fri Mar 20 15:55:07 2015 fernand veyrier
 */
 
 #include <stdlib.h>
@@ -24,13 +24,13 @@ double		*cubic_solver(double *param)
   result[1] = -1;
   result[2] = -1;
   p = (-pow(param[1], 2) / (3 * pow(param[0], 2))) + (param[2] / param[0]);
-  q = param[1] / (27 * param[0]) * ((2 * pow(param[1], 2)) / pow(param[0], 2) - (9 * param[2]) / param[0]) + (param[3] / param[0]);
+  q = param[1] / (27 * param[0]) * ((2 * pow(param[1], 2)) / pow(param[0], 2)
+				    - (9 * param[2]) / param[0]) + (param[3] / param[0]);
   delta = pow(q, 2) + (4 * pow(p, 3)) / 27;
-  printf("delta = %f p = %f q = %f\n", delta, p, q);
   if (delta > 0)
     {
-      result[0] = cbrt((-q + sqrt(delta)) / 2) + cbrt((-q - sqrt(delta)) / 2) - (param[1] / (3 * param[0]));
-      return (result);
+      result[0] = cbrt((-q + sqrt(delta)) / 2) + cbrt((-q - sqrt(delta)) / 2)
+	- (param[1] / (3 * param[0]));
     }
   else if (delta == 0)
     {
@@ -46,19 +46,23 @@ double		*cubic_solver(double *param)
     }
   else
     {
-      alpha = 1 / 3 * acos(delta);
-      result[0] = -param[1] / (3 * param[0]) + 2 * sqrt(-p / 3) * cos(alpha);
-      result[1] = -param[1] / (3 * param[0]) + 2 * sqrt(-p / 3) * cos(alpha + (2 * M_PI) / 3);
-      result[2] = -param[1] / (3 * param[0]) + 2 * sqrt(-p / 3) * cos(alpha + (4 * M_PI) / 3);
-      /* Et là, c'est le drame. */
-      printf("z1 = %f z2 = %f z3 = %f\n", result[0], result[1], result[2]);
-      return (result);
+      alpha = 1. / 3. * acos(-q / 2. * pow(27 / (pow(-p, 3.)), 0.5));
+      result[0] = -param[1] / (3 * param[0]) + 2. * sqrt(-p / 3.) * cos(alpha);
+      result[1] = -param[1] / (3 * param[0]) + 2. * sqrt(-p / 3.) * cos(alpha + (2. * M_PI) / 3.);
+      result[2] = -param[1] / (3 * param[0]) + 2. * sqrt(-p / 3.) * cos(alpha + (4. * M_PI) / 3.);
     }
+  return (result);
 }
 
 int		main(int ac, char **av)
 {
-  double	param[4] = {-2, 3, 0, -1};
+  double	param[4];
+  double	*result;
 
-  cubic_solver(param);
+  param[0] = atoi(av[1]);
+  param[1] = atoi(av[2]);
+  param[2] = atoi(av[3]);
+  param[3] = atoi(av[4]);
+  result = cubic_solver(param);
+  printf("z1 = %f z2 = %f z3 = %f\n", result[0], result[1], result[2]);
 }
