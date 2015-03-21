31dfee26aea1f8abe7679539c41daaa5ce57efc2
fernand veyrier <veyrie_f@pc-veyrie_f>
2015-03-21 20:29:54 +0100

    lolilol

--- sources/obj_norms_next.c
+++ sources/obj_norms_next.c
@@ -5,7 +5,7 @@
 ** Login   <veyrie_f@epitech.net>
 ** 
 ** Started on  Fri Mar 20 20:14:08 2015 fernand veyrier
-** Last update Sat Mar 21 18:01:26 2015 quentin gasparotto
+** Last update Sat Mar 21 20:09:34 2015 fernand veyrier
 */
 
 #include "../include/prototypes.h"
@@ -14,6 +14,7 @@ void		init_holed_cube_norm(t_vector3 *holed_cube_norm,
 				     t_vector3 isec_point, t_object *obj)
 {
   (void)obj;
+  printf("here\n");
   holed_cube_norm->x = 4 * pow(isec_point.x, 3) - 10 * isec_point.x;
   holed_cube_norm->y = 4 * pow(isec_point.y, 3) - 10 * isec_point.y;
   holed_cube_norm->z = 4 * pow(isec_point.z, 3) - 10 * isec_point.z;
