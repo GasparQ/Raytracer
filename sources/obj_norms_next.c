241604b672cd42c08532b861a82f23a66aa08567
Gasparotto <quentin.gasparotto@epitech.eu>
2015-03-24 18:46:58 +0100

    sans débug c mieu

--- sources/obj_norms_next.c
+++ sources/obj_norms_next.c
@@ -5,7 +5,7 @@
 ** Login   <veyrie_f@epitech.net>
 ** 
 ** Started on  Fri Mar 20 20:14:08 2015 fernand veyrier
-** Last update Tue Mar 24 18:44:17 2015 quentin gasparotto
+** Last update Tue Mar 24 18:46:21 2015 quentin gasparotto
 */
 
 #include "../include/prototypes.h"
@@ -14,7 +14,6 @@ void		init_holed_cube_norm(t_vector3 *holed_cube_norm,
 				     t_vector3 isec_point, t_object *obj)
 {
   (void)obj;
-  printf("here\n");
   holed_cube_norm->x = 4 * pow(isec_point.x, 3) - 10 * isec_point.x;
   holed_cube_norm->y = 4 * pow(isec_point.y, 3) - 10 * isec_point.y;
   holed_cube_norm->z = 4 * pow(isec_point.z, 3) - 10 * isec_point.z;
